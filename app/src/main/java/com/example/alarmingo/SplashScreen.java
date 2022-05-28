ed9a53f24ced2c578458ab578e6aa15fda16dca0
ZartDrag <kartikmay25@gmail.com>
2022-05-28 19:36:22 +0530

    Splash Screen with Animations added

---
+++ app/src/main/java/com/example/alarmingo/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.alarmingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    Animation up, down;
+    ImageView logoImage;
+    TextView AretaName, AretaCaption;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logoImage = findViewById(R.id.logo);
+        AretaName = findViewById(R.id.areta_name);
+        AretaCaption = findViewById(R.id.areta_caption);
+
+        up = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.up);
+        logoImage.setAnimation(up);
+
+        down = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.down);
+        AretaCaption.setAnimation(down);
+
+        new Handler().postDelayed(() -> {
+                startActivity(new Intent(getApplicationContext(),HomeScreen.class));
+        },4500);
+    }
+}
